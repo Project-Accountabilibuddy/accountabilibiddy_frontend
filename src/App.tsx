1b62c03abedaf00424b134725be43ac5397352b1
nfray <harry.fray7@gmail.com>
2022-12-11 12:18:09 -0500

    ui clean up for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,97 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import axios from "axios";
+import styled from "styled-components";
+import TextField from "@mui/material/TextField";
 
 import "./App.css";
 
-const App = () => {
-  const [everything, setEverything] = useState("");
+const makeid = (length: number) => {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
-  const handleGetEverything = () => {
+type Item = {
+  id: string;
+  name: string;
+};
+
+const StyledTodo = styled.div`
+  .create_components {
+    display: flex;
+    flex-direction: column;
+
+    input {
+      color: white;
+    }
+
+    button {
+      margin-top: 12px;
+    }
+  }
+
+  .items_title {
+    text-align: start;
+  }
+
+  .item {
+    display: flex;
+    flex-direction: row;
+    margin-top: 12px;
+    align-items: center;
+
+    h6 {
+      margin: 0;
+    }
+  }
+`;
+
+const App = () => {
+  const [allItems, setAllItems] = useState([]);
+  const [newItemName, setNewItemName] = useState("");
+
+  const getAllItems = () => {
     axios
       .get("https://euzdgtnwai.execute-api.us-east-1.amazonaws.com/items")
       .then((res) => {
         console.log(res?.data?.Items);
-        setEverything(JSON.stringify(res?.data?.Items));
+        setAllItems(res?.data?.Items);
+      })
+      .catch((err) => console.log({ err }));
+  };
+
+  useEffect(() => {
+    getAllItems();
+  }, []);
+
+  const handleDeleteItem = (id: string) => {
+    axios
+      .delete(
+        `https://euzdgtnwai.execute-api.us-east-1.amazonaws.com/items/${id}`
+      )
+      .then((res) => {
+        console.log(res);
+        getAllItems();
+      })
+      .catch((err) => console.log({ err }));
+  };
+
+  const handleCreateItem = () => {
+    axios
+      .put(`https://euzdgtnwai.execute-api.us-east-1.amazonaws.com/items`, {
+        id: makeid(4),
+        name: newItemName,
+      })
+      .then((res) => {
+        console.log(res);
+        getAllItems();
+        setNewItemName("");
       })
       .catch((err) => console.log({ err }));
   };
@@ -23,11 +101,31 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button onClick={handleGetEverything} variant="contained">
-          GET everyting!
-        </Button>
-        <h4>{everything}</h4>
+        <StyledTodo>
+          <h1>AWS CRUD</h1>
+          <h6>
+            S3 ~ Lambda ~ API Gateway ~ DynamoDB ~ Cloud Watch ~ Cloud Front
+          </h6>
+          <div className="create_components">
+            <TextField
+              variant="outlined"
+              value={newItemName}
+              onChange={(e) => setNewItemName(e.target.value)}
+            />
+            <Button onClick={handleCreateItem}>Create New Item</Button>
+          </div>
+          <h3 className="items_title">Your Items:</h3>
+          {allItems.map((item: Item, i) => {
+            return (
+              <div key={i} className="item">
+                <Button onClick={() => handleDeleteItem(item?.id)}>
+                  Delete
+                </Button>
+                <h6>{item?.name}</h6>
+              </div>
+            );
+          })}
+        </StyledTodo>
       </header>
     </div>
   );
