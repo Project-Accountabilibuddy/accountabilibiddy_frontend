ef290ccf19483965c4d1812c806a287d16400b9d
nfray <harry.fray7@gmail.com>
2022-12-11 08:50:26 -0500

    axios / material added / cors attempt fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [everything, setEverything] = useState("");
+
+  const handleGetEverything = () => {
+    axios
+      .get("https://euzdgtnwai.execute-api.us-east-1.amazonaws.com/items", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        },
+      })
+      .then((res) => {
+        console.log({ res });
+        setEverything(JSON.stringify(res));
+      })
+      .catch((err) => console.log({ err }));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button onClick={handleGetEverything} variant="contained">
+          GET everyting!
+        </Button>
+        <h1>{everything}</h1>
       </header>
     </div>
   );
-}
+};
 
 export default App;
