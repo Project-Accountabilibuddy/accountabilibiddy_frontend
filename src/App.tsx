db0fce9fd9e74b7d411f3f4c85e4650b7d24b4ac
nfray <harry.fray7@gmail.com>
2022-12-16 18:54:03 -0700

    check for authed user

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import { Route, Routes } from "react-router-dom";
+import { Auth } from "aws-amplify";
 
 import CrudPage from "./pages/Crud";
 import AuthPage from "./pages/Auth";
 import "./App.css";
 
 const App = () => {
+  Auth.currentAuthenticatedUser({
+    bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+  })
+    .then((user) => console.log(user))
+    .catch((err) => console.log(err));
+
   return (
     <div className="App">
       <header className="App-header">
