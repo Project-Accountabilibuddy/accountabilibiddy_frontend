d3b0e4f5fbfd971cc223576e3028ed016f690348
nfray <harry.fray7@gmail.com>
2023-01-05 11:07:05 -0600

    zu set up and used

---
+++ src/global/GlobalSate.tsx
@@ -0,0 +1,56 @@
+import create from "zustand";
+
+interface GlobalState {
+  userResponseWhatLongForm: string;
+  userResponseSacrificeLongForm: string;
+  projectName: string;
+  userResponseWhyLongForm: string;
+  userResponseWhyShortForm: string;
+  userResponseHattersLongForm: string;
+  userResponseHattersShortForm: string;
+
+  setUserResponseWhatLongForm: (userResponseWhatLongForm: string) => void;
+  setUserResponseSacrificeLongForm: (
+    userResponseSacrificeLongForm: string
+  ) => void;
+  setProjectName: (projectName: string) => void;
+  setUserResponseWhyLongForm: (userResponseWhyLongForm: string) => void;
+  setUserResponseWhyShortForm: (userResponseWhyShortForm: string) => void;
+  setUserResponseHattersLongForm: (userResponseHattersLongForm: string) => void;
+  setUserResponseHattersShortForm: (
+    userResponseHattersShortForm: string
+  ) => void;
+}
+
+const useGlobalState = create<GlobalState>((set) => ({
+  userResponseWhatLongForm: "",
+  setUserResponseWhatLongForm: (userResponseWhatLongForm) =>
+    set(() => ({ userResponseWhatLongForm })),
+
+  userResponseSacrificeLongForm: "",
+  setUserResponseSacrificeLongForm: (userResponseSacrificeLongForm) =>
+    set(() => ({ userResponseSacrificeLongForm })),
+
+  projectName: "",
+  setProjectName: (projectName) => set(() => ({ projectName })),
+
+  userResponseWhyLongForm: "",
+  setUserResponseWhyLongForm: (userResponseWhyLongForm) =>
+    set(() => ({ userResponseWhyLongForm })),
+
+  userResponseWhyShortForm: "",
+  setUserResponseWhyShortForm: (userResponseWhyShortForm) =>
+    set(() => ({ userResponseWhyShortForm })),
+
+  userResponseHattersLongForm: "",
+  setUserResponseHattersLongForm: (userResponseHattersLongForm) =>
+    set(() => ({ userResponseHattersLongForm })),
+
+  userResponseHattersShortForm: "",
+  setUserResponseHattersShortForm: (userResponseHattersShortForm) =>
+    set(() => ({ userResponseHattersShortForm })),
+
+  userResponseWeeksToAccomplish: 1,
+}));
+
+export default useGlobalState;
