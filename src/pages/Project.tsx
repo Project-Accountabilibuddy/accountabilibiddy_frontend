da1c99a80110a158df4538944391fc1a407ffea3
nfray <harry.fray7@gmail.com>
2023-01-05 12:20:03 -0600

    progress

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -19,6 +19,7 @@ const StyledProject = styled.div`
   text-align: center;
   justify-content: center;
   width: 100%;
+  height: 100%;
 
   .top_nav_bar {
     display: flex;
@@ -27,6 +28,37 @@ const StyledProject = styled.div`
     height: 60px;
     padding: 0 24px;
     background-color: ${({ theme }) => theme.colors.lightGrey};
+  }
+
+  .three_sections {
+    display: flex;
+    height: 100%;
+
+    .section_one,
+    .section_two,
+    .section_three {
+      padding: 24px;
+      width: 100%;
+      border: 2px solid ${({ theme }) => theme.colors.secondary};
+      border-radius: 8px;
+    }
+
+    .section_one,
+    .section_three {
+      width: 50%;
+    }
+
+    .section_one {
+      margin: 12px 6px 12px 12px;
+    }
+
+    .section_two {
+      margin: 12px 6px 12px 6px;
+    }
+
+    .section_three {
+      margin: 12px 12px 12px 6px;
+    }
   }
 
   .weekly_input_form {
@@ -89,10 +121,19 @@ const Project = () => {
     <StyledProject>
       <div className="top_nav_bar">
         <h1 className="heading-1">{projectName}</h1>
-        <h1 className="heading-1">Accountabilibuddy</h1>
+        <h1 className="heading-1 future_logo">Accountabilibuddy</h1>
         <Button onClick={handleSignOut}>Sign Out</Button>
       </div>
-      <div className="weekly_input_form">
+      <div className="three_sections">
+        <div className="section_one">
+          <h3 className="body-2">{userResponseWhyLongForm}</h3>
+        </div>
+        <div className="section_two"></div>
+        <div className="section_three">
+          <h3 className="body-2">{userResponseHattersLongForm}</h3>
+        </div>
+      </div>
+      {/* <div className="weekly_input_form">
         <TextField
           variant="outlined"
           value={newItemName}
@@ -108,7 +149,7 @@ const Project = () => {
             <h6>{item?.name}</h6>
           </div>
         );
-      })}
+      })} */}
     </StyledProject>
   );
 };
