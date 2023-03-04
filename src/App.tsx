7686bdebb90ccc1617328afa3e1bc8713ded76cd
nfray <harry.fray7@gmail.com>
2023-03-04 12:32:06 -0700

    clean up lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import React, { useEffect, lazy, Suspense } from 'react'
 import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
-// import { Auth } from 'aws-amplify'
-// import CircularProgress from '@mui/material/CircularProgress'
+import { Auth } from 'aws-amplify'
+import CircularProgress from '@mui/material/CircularProgress'
 import styled from 'styled-components'
 
 import GlobalTheme from './global/GlobalTheme'
 import GlobalTypography from './global/GlobalTypography'
-// import useBackEndMethods from './hooks/useBackEndMethods'
-// import useGlobalState from './global/GlobalSate'
+import useBackEndMethods from './hooks/useBackEndMethods'
+import useGlobalState from './global/GlobalSate'
 import { ROUTES } from './global/Constants'
 
 import LogoBig from './icons/LogoBig'
 import { MountainforegroundImage, SunBackgroundImage } from './images'
 
 const LandingPage = lazy(async () => await import('./pages/Landing'))
-// const ProjectSetUpPage = lazy(async () => await import('./pages/ProjectSetup'))
-// const ProjectPage = lazy(async () => await import('./pages/Project'))
-// const AuthPage = lazy(async () => await import('./pages/Auth'))
+const ProjectSetUpPage = lazy(async () => await import('./pages/ProjectSetup'))
+const ProjectPage = lazy(async () => await import('./pages/Project'))
+const AuthPage = lazy(async () => await import('./pages/Auth'))
 
 const StyledApp = styled.div`
-  // background-image: url(${SunBackgroundImage});
+  background-image: url(${SunBackgroundImage});
   background-size: auto 100%;
   background-position: center;
   background-color: var(--color-background);
 
   .foreground {
-    // background-image: url(${MountainforegroundImage});
+    background-image: url(${MountainforegroundImage});
     background-size: auto 100%;
     animation: animatedBackground 200s linear infinite;
 
@@ -74,54 +74,54 @@ const StyledGlobalLoading = styled.div`
 `
 
 const App = (): JSX.Element => {
-  // const { handleGetProjects } = useBackEndMethods()
-  // const { globalLoading, setGlobalLoading, setUserName } = useGlobalState()
+  const { handleGetProjects } = useBackEndMethods()
+  const { globalLoading, setGlobalLoading, setUserName } = useGlobalState()
 
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
   const { pathname } = useLocation()
 
-  // useEffect(() => {
-  //   const autoNavigateUserBasedOnAuth = async (): Promise<any> => {
-  //     try {
-  //       await Auth.currentAuthenticatedUser()
-  //       // DIRECT USER INTO AUTHED ROUTES IF SIGNED IN
-  //       if (pathname === '/' || pathname === ROUTES.AUTH) {
-  //         navigate(ROUTES.PROJECT)
-  //       }
-  //     } catch (err) {
-  //       // KICK USER OUT OF AUTHED ROUTES IF NOT SIGNED IN
-  //       if (pathname === ROUTES.PROJECT) {
-  //         navigate('/')
-  //       }
-  //     }
-  //   }
+  useEffect(() => {
+    const autoNavigateUserBasedOnAuth = async (): Promise<any> => {
+      try {
+        await Auth.currentAuthenticatedUser()
+        // DIRECT USER INTO AUTHED ROUTES IF SIGNED IN
+        if (pathname === '/' || pathname === ROUTES.AUTH) {
+          navigate(ROUTES.PROJECT)
+        }
+      } catch (err) {
+        // KICK USER OUT OF AUTHED ROUTES IF NOT SIGNED IN
+        if (pathname === ROUTES.PROJECT) {
+          navigate('/')
+        }
+      }
+    }
 
-  //   autoNavigateUserBasedOnAuth().finally(() => {
-  //     console.log('autoNavigateUserBasedOnAuth finished')
-  //   })
-  // }, [navigate, pathname])
+    autoNavigateUserBasedOnAuth().finally(() => {
+      console.log('autoNavigateUserBasedOnAuth finished')
+    })
+  }, [navigate, pathname])
 
-  // useEffect(() => {
-  //   const getUserAndProjectState = async (): Promise<any> => {
-  //     try {
-  //       setGlobalLoading(true)
-  //       const response = await Auth.currentAuthenticatedUser({
-  //         bypassCache: true
-  //       })
-  //       setUserName(response.attributes.email)
-  //       await handleGetProjects(() => {
-  //         setGlobalLoading(false)
-  //       })
-  //     } catch (err) {
-  //       console.log({ err })
-  //       setGlobalLoading(false)
-  //     }
-  //   }
+  useEffect(() => {
+    const getUserAndProjectState = async (): Promise<any> => {
+      try {
+        setGlobalLoading(true)
+        const response = await Auth.currentAuthenticatedUser({
+          bypassCache: true
+        })
+        setUserName(response.attributes.email)
+        await handleGetProjects(() => {
+          setGlobalLoading(false)
+        })
+      } catch (err) {
+        console.log({ err })
+        setGlobalLoading(false)
+      }
+    }
 
-  //   getUserAndProjectState().finally(() => {
-  //     console.log('getUserAndProjectState finished')
-  //   })
-  // }, [])
+    getUserAndProjectState().finally(() => {
+      console.log('getUserAndProjectState finished')
+    })
+  }, [])
 
   const showLogo = pathname === '/' || pathname === ROUTES.AUTH
 
@@ -132,29 +132,27 @@ const App = (): JSX.Element => {
           <div className="foreground">
             <div className="content">
               {showLogo && <LogoBig className="logo" />}
-              {/* {globalLoading && (
+              {globalLoading && (
                 <StyledGlobalLoading>
                   <CircularProgress />
                 </StyledGlobalLoading>
-              )} */}
-              <LandingPage />
-              {/* <Suspense
-                fallback={
-                  <StyledGlobalLoading>
-                    <CircularProgress />
-                  </StyledGlobalLoading>
-                }
-              >
-                <Routes>
-                  <Route path="/" element={<LandingPage />} />
-                  <Route
-                    path={`${ROUTES.PROJECT_SETUP}/*`}
-                    element={<ProjectSetUpPage />}
-                  />
-                  <Route path={`${ROUTES.AUTH}/*`} element={<AuthPage />} />
-                  <Route path={`${ROUTES.PROJECT}`} element={<ProjectPage />} />
-                </Routes>
-              </Suspense> */}
+              )}
+              {!globalLoading && (
+                <Suspense>
+                  <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route
+                      path={`${ROUTES.PROJECT_SETUP}/*`}
+                      element={<ProjectSetUpPage />}
+                    />
+                    <Route path={`${ROUTES.AUTH}/*`} element={<AuthPage />} />
+                    <Route
+                      path={`${ROUTES.PROJECT}`}
+                      element={<ProjectPage />}
+                    />
+                  </Routes>
+                </Suspense>
+              )}
             </div>
           </div>
         </StyledApp>
