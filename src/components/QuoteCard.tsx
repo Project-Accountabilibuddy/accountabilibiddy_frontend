ef653c10c07ec851b092f39ce03c2990a289e6c0
nfray <harry.fray7@gmail.com>
2023-03-30 09:13:34 -0600

    quote card created and simple grid implemented

---
+++ src/components/QuoteCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface QuoteCardProps {
+  quoteTitle?: string
+  subquoteTitle?: string
+  className: string
+}
+
+const StyledQuoteCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 14rem;
+  height: 10rem;
+  border: 1px solid var(--color-primary);
+  border-radius: 2px;
+
+  .quote_title {
+    margin-bottom: 8px;
+  }
+
+  .subquote_title {
+    color: var(--color-light-grey);
+  }
+`
+
+const QuoteCard = ({
+  quoteTitle = 'todo',
+  subquoteTitle = 'todo'
+}: QuoteCardProps): JSX.Element => {
+  return (
+    <StyledQuoteCard>
+      <h4 className="caption quote_title">{quoteTitle}</h4>
+      <h4 className="caption subquote_title">{subquoteTitle}</h4>
+    </StyledQuoteCard>
+  )
+}
+
+export default QuoteCard
--- src/pages/Quotes.tsx
+++ src/pages/Quotes.tsx
@@ -3,6 +3,13 @@ import styled from 'styled-components'
 import axios from 'axios'
 
 import Button from '../components/Button'
+import QuoteCard from '../components/QuoteCard'
+
+interface Quote {
+  q: string
+  a: string
+  h: string
+}
 
 const StyledQuotes = styled.div`
   display: flex;
@@ -13,24 +20,34 @@ const StyledQuotes = styled.div`
 
   .title {
     margin-bottom: 48px;
-    font-size: 5rem;
-  }
-
-  .subheading_text {
-    margin-bottom: 48px;
-    color: var(--color-light-grey);
   }
 
   .quotes {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 12px;
+    margin-bottom: 24px;
+  }
+
+  @media (max-width: 1024px) {
+    .quotes {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
+  @media (max-width: 700px) {
+    .quotes {
+      grid-template-columns: repeat(2, 1fr);
+    }
   }
 `
 
+let DUMMY_QUOTES = new Array(12).fill(0)
+
 const Quotes = (): JSX.Element => {
+  const [quotes, setQuotes] = React.useState<Quote[]>(DUMMY_QUOTES)
+
+  // todo: auto call on filter change
   const handleGetQuotes = async (): Promise<any> => {
     try {
       const response = await fetch('https://zenquotes.io/api/quotes/')
@@ -44,10 +61,12 @@ const Quotes = (): JSX.Element => {
   return (
     <StyledQuotes>
       <h1 className="heading-1 title">Quotes</h1>
-      <h4 className="heading-1 subheading_text">
-        Here are some quotes about love
-      </h4>
-      <div className="quotes"></div>
+      <div className="quotes">
+        {quotes.map((quote, index) => {
+          return <QuoteCard key={index} className={'quote'} />
+        })}
+      </div>
+
       <Button
         variant="outlined"
         onClick={() => handleGetQuotes()}
